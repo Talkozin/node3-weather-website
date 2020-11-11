69f91c4813caa8ce6535bf6bbdce5db762d518b3
Tal Kozin <blackvolt101@gmail.com>
2020-11-11 17:41:20 +0200

    Init commit3

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -11,7 +11,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+ 
 
 weatherForm.addEventListener('submit', (e) => {
 
